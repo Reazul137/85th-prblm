a6372ec10192eb8426b84bece125ef5398ee1784
Reazul Islam <reazulislam137@gmail.com>
2018-03-30 12:07:45 +0600

    first commit

---
+++ march-30-01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Input a number : ");
+    scanf("%d",&num);
+
+    if(num >= 0)
+        printf("%d is a positive number!");
+    else
+        printf("%d is a negative number!");
+
+    return 0;
+}
